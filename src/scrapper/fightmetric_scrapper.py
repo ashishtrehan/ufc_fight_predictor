5c2f297706f5aa49d7a132af7b98c6e3f4ebaebb
Ashish Trehan <ashishtrehan10@gmail.com>
2018-10-17 14:54:22 -0400

    adding models.py and iterate_scrape

--- src/scrapper/fightmetric_scrapper.py
+++ src/scrapper/fightmetric_scrapper.py
@@ -29,16 +29,28 @@ def obtain_urls():
             if url_cleaner(x.get("href")) is not None]
     return text
 
+def event_date_scaper(soup):
+    for i in soup.find_all('ul'):
+        for idx, li in enumerate(i.findChildren('li')):
+            if idx in range(3):
+                if str.find(str(li.text), r'Date:') != -1:
+                    event_date = ' '.join(str(li.text).split())  # split then rejoin to strip whitespace properly
+                    event_date = event_date[6:]
+                    #TODO replace pandas with datetime or dateutil package
+                    event_date = pd.to_datetime(event_date)
+    return event_date
 
-# all_links = soup.find_all("a")
-# for link in all_links:
-#     link_text = link.get("href")
-#     if not pd.isnull(link_text):
-#         try:
-#             link_text = link_text.encode('ascii','ignore') #fix issue of str(link_text) not working due to unknown unicode character
-#         except:
-#             print (link_text)
-#         if str.find(str(link_text),r'http://www.fightmetric.com/event-details/') != -1:
-#             urls_list.append(link_text)
+def iterate_scrape(list):
+    for x in list:
+        page = requests.get(x)
+        soup = BeautifulSoup(page.content)
+        event_date = None
+        event_date = event_date_scaper(soup)
+        event_title = soup.find_all('span', class_=r'b-content__title-highlight')[0].text
+        event_title = ' '.join(str(event_title).split())
+        print (event_title)
 
-print (obtain_urls())
+
+
+
+iterate_scrape(obtain_urls())
